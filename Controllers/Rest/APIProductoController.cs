4ed12aa75050a72509bb1d2a6c5348c47a076ae8
Nayla Huamani Hancco <yamill_huamani@usmp.pe>
2020-12-19 08:08:08 -0500

    crear Api

--- Controllers/Rest/APIProductoController.cs
+++ Controllers/Rest/APIProductoController.cs
@@ -9,5 +9,18 @@ using Jamui_Rantiy.Data;
 
 namespace Jamui_Rantiy.Controllers.Rest
 {
-   
-}
+   [ApiController]
+    [Route("api/editar")]
+    public class APIProductoController : ControllerBase
+    {
+       private readonly ILogger<APIProductoController> _logger;
+       private readonly ApplicationDbContext _context;
+
+        public APIProductoController(ILogger<APIProductoController> logger,
+            ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+    }
+    }
