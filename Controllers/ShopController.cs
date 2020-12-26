1e2df8740aae059037e023f1df8c6d79c7c24174
Nayla Huamani Hancco <yamill_huamani@usmp.pe>
2020-12-26 09:47:12 -0500

    Se agrego el usuarios

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -5,28 +5,107 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Jamui_Rantiy.Models;
+using Jamui_Rantiy.Data;
+using System.Dynamic;
 
 namespace Jamui_Rantiy.Controllers
 {
     public class ShopController : Controller
     {
-        private readonly ILogger<ShopController> _logger;
 
-        public ShopController(ILogger<ShopController> logger)
+       private readonly ILogger<ShopController> _logger;
+       private readonly ApplicationDbContext _context;
+
+
+        public ShopController(ILogger<ShopController> logger,
+            ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var listContactos=_context.Contactos.ToList();
+            return View(listContactos);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Create()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var listProductos=_context.Productos.ToList();
+            Contacto contacto = new Contacto();
+            dynamic model = new ExpandoObject();
+            model.contacto = contacto;
+            model.productos = listProductos;
+            return View(model);
         }
+
+        [HttpPost]
+        public IActionResult Create(Contacto objContacto){
+            if (ModelState.IsValid)
+            {
+                _context.Add(objContacto);
+                _context.SaveChanges();
+                objContacto.Response = "Gracias estamos en contacto";
+            }
+            return View(objContacto);
+        }
+
+        // GET: Contacto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = await _context.Contactos.FindAsync(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return View(contacto);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,LastName,Email,Phone")] Contacto contacto)
+        {
+            if (id != contacto.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contacto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contacto);
+        }
+        
+
+        // GET: http://localhost:5000/Contacto/Delete/6 MUESTRA Contacto
+        public IActionResult Delete(int? id)
+        {
+            var contacto = _context.Contactos.Find(id);
+            _context.Contactos.Remove(contacto);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
