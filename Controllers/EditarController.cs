252117ebfbd521f177c037b773e23fbf6e84b7f8
Patricia Zambrano <pzambranocarpio@gmail.com>
2021-01-01 23:46:35 -0500

    delete and edit

--- Controllers/EditarController.cs
+++ Controllers/EditarController.cs
@@ -2,6 +2,14 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Jamui_Rantiy.Data;
+using System.Threading.Tasks;
+using Jamui_Rantiy.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Jamui_Rantiy.Controllers
 {
@@ -24,7 +32,54 @@ namespace Jamui_Rantiy.Controllers
             var listProductos=_context.Productos.ToList();
             return View(listProductos);
         }
+public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return View(producto);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,image_name,price")] Producto producto)
+        {
+            if (id != producto.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                    
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
+        }
+    
+        public IActionResult Delete(int? id)
+        {
+            var contacto = _context.Contactos.Find(id);
+            _context.Productos.Remove(producto);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
 
     }
 }
