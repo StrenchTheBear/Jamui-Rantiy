d5c0787897ffbff64d4e678d6a3f53435a0c9f47
Patricia Zambrano <pzambranocarpio@gmail.com>
2021-01-02 02:33:57 -0500

    cambios api

--- Controllers/Rest/APIProductoController.cs
+++ Controllers/Rest/APIProductoController.cs
@@ -9,41 +9,43 @@ using Jamui_Rantiy.Data;
 
 namespace Jamui_Rantiy.Controllers.Rest
 {
-  [ApiController]
-    [Route("api/productos")]
-    public class APIProductoController : ControllerBase
-    {
-       private readonly ILogger<APIProductoController> _logger;
-       private readonly ApplicationDbContext _context;
+ 
+    //  [ApiController]
+    // [Route("api/productos")]
+    // public class APIProductoController : ControllerBase
+    // {
+    //    private readonly ILogger<APIProductoController> _logger;
+    //    private readonly ApplicationDbContext _context;
 
-        public APIProductoController(ILogger<APIProductoController> logger,
-            ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
+    //     public APIProductoController(ILogger<APIProductoController> logger,
+    //         ApplicationDbContext context)
+    //     {
+    //         _logger = logger;
+    //         _context = context;
+    //     }
 
 
-        [HttpGet]
+    //     [HttpGet]
         
-        public IEnumerable<Producto> ListProductos()
-        {
-             var listProductos=_context.Productos.OrderBy(s => s.ID).ToList();   
-             return listProductos.ToArray();
-        }
+    //     public IEnumerable<Producto> ListProductos()
+    //     {
+    //          var listProductos=_context.Productos.OrderBy(s => s.ID).ToList();   
+    //          return listProductos.ToArray();
+    //     }
 
-        [HttpGet("{id}")]
-        public Producto GetProduct(int? id)
-        {
-            var producto =  _context.Productos.Find(id);
-            return producto;
-        }
+    //     [HttpGet("{id}")]
+    //     public Producto GetProduct(int? id)
+    //     {
+    //         var producto =  _context.Productos.Find(id);
+    //         return producto;
+    //     }
 
-        [HttpPost]
-        public Producto CreateProduct(Producto producto){
-            _context.Add(producto);
-            _context.SaveChanges();
-            return producto;
-        }
-    }
+    //     [HttpPost]
+    //     public Producto CreateProduct(Producto producto){
+    //         _context.Add(producto);
+    //         _context.SaveChanges();
+    //         return producto;
+    //     }
+    // } 
+  
 }
