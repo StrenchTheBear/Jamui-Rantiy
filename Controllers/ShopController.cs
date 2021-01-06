d2a91b35a19b8e45445478147d0006761cc0546d
StrenchTheBear <acliv2904@yahoo.com>
2021-01-06 14:55:18 -0500

    Se agrego Shop

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -15,7 +15,23 @@ namespace Jamui_Rantiy.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly ILogger<ShopController> _logger;
 
-       
+        public ShopController(ILogger<ShopController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
